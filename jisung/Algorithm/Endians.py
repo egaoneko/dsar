78be453333bc7b02e0a69995184de9f311461a95
jisung <cbajs20@gmail.com>
2015-09-08 16:57:45 +0900

    jisung edit

--- jisung/Algorithm/Endians.py
+++ jisung/Algorithm/Endians.py
@@ -2,11 +2,18 @@ __author__ = 'jeonjiseong'
 import struct
 import sys
 
+r1 = lambda : sys.stdin.readline()
+n = int(r1())
+input = list()
+
+for i in range(n):
+    input.append(int(r1()))
+
 def endians(number):
-    endi = sys.byteorder
+    endi = sys.byteorder # 입력 값이 빅 엔디안인지 리틀엔디안 인지 판별 -> return 값은 little 또는 big의 스트링값
     if endi == 'little' :
-        ltb = struct.pack('>I',number)
-        bnum = struct.unpack('I',ltb)
+        ltb = struct.pack('>I',number) # struct.pack의 매개변수 >는 빅엔디안으로의 변환을 의미 I는 unsigned int형을 의미 >는 리틀엔디안으로의 변환을 의
+        bnum = struct.unpack('I',ltb) # pack은 값을 매개변수의 format에 따라 변경, unpack은 그 반대를 실행
         #print repr(ltb)
         return bnum[0]
     else:
@@ -14,4 +21,5 @@ def endians(number):
         lnum = struct.unpack('I',btl)
         return lnum[0]
 
-print endians(4294967295)
+for x in input:
+    print endians(x)
--- jisung/Algorithm/mercy.py
+++ jisung/Algorithm/mercy.py
@@ -1,11 +1,15 @@
 __author__ = 'jeonjiseong'
+import sys
+
+r1 = lambda : sys.stdin.readline()
+a = int(r1())
 
 def mercy(a):
     if a<1 or a>10:
         print('Not allowed range of numer')
     else:
         while a != 0:
-            print 'Hello Algospot'
+            print 'Hello Algospot!'
             a -= 1
 
-mercy(3)
+mercy(a)
